0341ccd18388b9553ca915c63a19f0eada69d828
Chin Yu <chin.yuchiuj@gmail.com>
2021-08-25 15:29:41 +0800

    account has been blocked

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 password = 'a123456'
 i = 3 # sign in times
 while i > 0:
+	i = i -1 
 	pwd = input('enter your password')
 	if pwd == password:
 		print('sign in')
 		break #salir de loop
 	else:
-		i = i -1 
-		print('password error', 'remain', i , 'time to try')
+		print('password error')
+		if i > 0:
+			print('remain', i , 'time to try')
+		else:
+			print('account has been blocked')
